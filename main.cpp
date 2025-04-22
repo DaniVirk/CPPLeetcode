107fbc1644ac1e29986fb9f655c9681ef76354fb
Danial <dani2630dk@gmail.com>
2025-04-22 17:28:13 +0200

    init

--- main.cpp
+++ main.cpp
@@ -3,10 +3,5 @@
 #include "src/headers/longest_valid_parentheses.h"
 
 int main() {
-    longest_valid_parentheses lvp;
-
-    const int test = lvp.longestValidParentheses(")()())");
-
-    std::cout << test << std::endl;
     return 0;
 }
