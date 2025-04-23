0536179172f256e8b1fc97d838e5dc2cafc3d8f2
Danial <dani2630dk@gmail.com>
2025-04-24 00:34:52 +0200

    reverse int

---
+++ src/Leetcode/reverse_integer.cpp
@@ -0,0 +1,23 @@
+﻿#include "../headers/reverse_integer.h"
+#include <string>
+
+int reverse_integer::reverse(int x) {
+    const std::string x_string = std::to_string(x);
+    std::string x_reversed;
+    int start_index = 0;
+
+    if (x_string[0] == '-') {
+        x_reversed = "-";
+        start_index = 1;
+    }
+
+    for (int i = x_string.size() - 1; i >= start_index; --i) {
+        x_reversed += x_string[i];
+    }
+
+    try {
+        return std::stoi(x_reversed);
+    } catch (...) {
+        return 0;
+    }
+}
---
+++ src/headers/reverse_integer.h
@@ -0,0 +1,13 @@
+﻿#ifndef REVERSE_INTEGER_H
+#define REVERSE_INTEGER_H
+
+
+
+class reverse_integer {
+public:
+    int reverse(int x);
+};
+
+
+
+#endif //REVERSE_INTEGER_H
