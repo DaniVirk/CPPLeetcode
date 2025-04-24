1727ebc06d495c32506a1f30cd913bd879155fda
Danial <dani2630dk@gmail.com>
2025-04-24 02:26:26 +0200

    merge two sorted lists

---
+++ src/Leetcode/merge_two_sorted_lists.cpp
@@ -0,0 +1,15 @@
+﻿#include "../headers/merge_two_sorted_lists.h"
+#include "../helper/ListNode.h"
+
+ListNode *merge_two_sorted_lists::mergeTwoLists(ListNode *list1, ListNode *list2) {
+    if (list1 == nullptr && list2 == nullptr) return nullptr;z
+    if (list1 == nullptr && list2 != nullptr) return list2;
+    if (list1 != nullptr && list2 == nullptr) return list1;
+
+    if (list1->val <= list2->val) {
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
+    }
+    list2->next = mergeTwoLists(list1, list2->next);
+    return list2;
+}
---
+++ src/headers/merge_two_sorted_lists.h
@@ -0,0 +1,13 @@
+﻿#ifndef MERGE_TWO_SORTED_LISTS_H
+#define MERGE_TWO_SORTED_LISTS_H
+#include "../helper/ListNode.h"
+
+
+class merge_two_sorted_lists {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
+};
+
+
+
+#endif //MERGE_TWO_SORTED_LISTS_H
