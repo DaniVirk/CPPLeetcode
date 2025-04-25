0eeaf7d78cb487d1ab3461c8f9eeea231e49a2ae
Danial <dani2630dk@gmail.com>
2025-04-25 13:39:43 +0200

    linked_list_cycle

---
+++ src/Leetcode/linked_list_cycle.cpp
@@ -0,0 +1,13 @@
+﻿#include "../headers/linked_list_cycle.h"
+
+bool linked_list_cycle::hasCycle(ListNode *head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
---
+++ src/headers/linked_list_cycle.h
@@ -0,0 +1,13 @@
+﻿#ifndef LINKED_LIST_CYCLE_H
+#define LINKED_LIST_CYCLE_H
+
+#include "../helper/ListNode.h"
+
+class linked_list_cycle {
+public:
+    bool hasCycle(ListNode *head);
+};
+
+
+
+#endif //LINKED_LIST_CYCLE_H
