25dc01a9b8f0cf5e3823e35487934216b687f284
Danial <dani2630dk@gmail.com>
2025-04-25 14:00:36 +0200

    linked_list_cycle

---
+++ src/Leetcode/find_the_difference.cpp
@@ -0,0 +1,16 @@
+﻿#include "../headers/find_the_difference.h"
+
+#include <algorithm>
+
+char find_the_difference::findTheDifference(string s, string t) {
+    std::sort(s.begin(), s.end());
+    std::sort(t.begin(), t.end());
+
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] != t[i]) {
+            return t[i];
+        }
+    }
+
+    return t.back();
+}
---
+++ src/headers/find_the_difference.h
@@ -0,0 +1,14 @@
+﻿#ifndef FIND_THE_DIFFERENCE_H
+#define FIND_THE_DIFFERENCE_H
+#include <string>
+
+using std::string;
+
+class find_the_difference {
+public:
+    char findTheDifference(string s, string t);
+};
+
+
+
+#endif //FIND_THE_DIFFERENCE_H
