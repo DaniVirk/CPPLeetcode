9a648e10937008b15537ba961b6b6b0cf7a4812b
Danial <dani2630dk@gmail.com>
2025-04-26 10:52:42 +0200

    firstUniqChar

---
+++ src/Leetcode/first_unique_character_in_a_string.cpp
@@ -0,0 +1,19 @@
+﻿#include "../headers/first_unique_character_in_a_string.h"
+
+#include <algorithm>
+#include <map>
+
+int first_unique_character_in_a_string::firstUniqChar(string s) {
+    std::map<char, int> matched;
+
+    for (char & i : s) {
+        matched[i]++;
+    }
+
+    for (int i = 0; i < s.size(); ++i) {
+        if (matched[s[i]] == 1) {
+            return i;
+        }
+    }
+    return -1;
+}
---
+++ src/headers/first_unique_character_in_a_string.h
@@ -0,0 +1,14 @@
+﻿#ifndef FIRST_UNIQUE_CHARACTER_IN_A_STRING_H
+#define FIRST_UNIQUE_CHARACTER_IN_A_STRING_H
+#include <string>
+using std::string;
+
+
+class first_unique_character_in_a_string {
+public:
+    int firstUniqChar(string s);
+};
+
+
+
+#endif //FIRST_UNIQUE_CHARACTER_IN_A_STRING_H
