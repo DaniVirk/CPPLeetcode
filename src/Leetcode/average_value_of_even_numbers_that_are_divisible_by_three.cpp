bf15ab671ee18529c4487c6c4c1afaea9e4ae38d
Danial <dani2630dk@gmail.com>
2025-04-27 01:50:49 +0200

    averageValue

---
+++ src/Leetcode/average_value_of_even_numbers_that_are_divisible_by_three.cpp
@@ -0,0 +1,15 @@
+﻿#include "../headers/average_value_of_even_numbers_that_are_divisible_by_three.h"
+
+int average_value_of_even_numbers_that_are_divisible_by_three::averageValue(vector<int> &nums) {
+    int number = 0;
+    int counter = 0;
+
+    for (const int num : nums) {
+        if (num % 3 == 0 && num % 2 == 0) {
+            number += num;
+            counter++;
+        }
+    }
+    if (counter == 0)  return number;
+    return number / counter;
+}
---
+++ src/headers/average_value_of_even_numbers_that_are_divisible_by_three.h
@@ -0,0 +1,14 @@
+﻿#ifndef AVERAGE_VALUE_OF_EVEN_NUMBERS_THAT_ARE_DIVISIBLE_BY_THREE_H
+#define AVERAGE_VALUE_OF_EVEN_NUMBERS_THAT_ARE_DIVISIBLE_BY_THREE_H
+#include <vector>
+
+using std::vector;
+
+class average_value_of_even_numbers_that_are_divisible_by_three {
+public:
+    static int averageValue(vector<int>& nums);
+};
+
+
+
+#endif //AVERAGE_VALUE_OF_EVEN_NUMBERS_THAT_ARE_DIVISIBLE_BY_THREE_H
