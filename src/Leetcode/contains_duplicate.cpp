607e6bf168c966bcdaa7c82718065c38dd416d8d
Danial <dani2630dk@gmail.com>
2025-04-27 10:07:57 +0200

    hasDuplicate

---
+++ src/Leetcode/contains_duplicate.cpp
@@ -0,0 +1,16 @@
+﻿#include "../headers/contains_duplicate.h"
+
+#include <unordered_map>
+
+bool contains_duplicate::hasDuplicate(vector<int> &nums) {
+    std::unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        if (mp.find(nums[i]) != mp.end()) {
+            return true;
+        }
+        mp[nums[i]]++;
+    }
+
+    return false;
+}
---
+++ src/headers/contains_duplicate.h
@@ -0,0 +1,14 @@
+﻿#ifndef CONTAINS_DUPLICATE_H
+#define CONTAINS_DUPLICATE_H
+#include <vector>
+
+using std::vector;
+
+class contains_duplicate {
+public:
+    static bool hasDuplicate(vector<int>& nums);
+};
+
+
+
+#endif //CONTAINS_DUPLICATE_H
