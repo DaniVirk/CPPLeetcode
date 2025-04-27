0b2d22c1f53e47fa09686ed0477d8895cbbc7c35
Danial <dani2630dk@gmail.com>
2025-04-27 02:27:41 +0200

    removeNthFromEnd

---
+++ src/Leetcode/remove_nth_node_from_end_of_list.cpp
@@ -0,0 +1,37 @@
+﻿#include "../headers/remove_nth_node_from_end_of_list.h"
+
+ListNode *remove_nth_node_from_end_of_list::removeNthFromEnd(ListNode *head, int n) {
+    if (head == nullptr) return nullptr;
+    if (head->next == nullptr && n == 1) return nullptr;
+
+    int length = 0;
+    ListNode* temp = head;
+    while (temp != nullptr) {
+        length++;
+        temp = temp->next;
+    }
+
+    if (n == length) {
+        return head->next;
+    }
+
+    auto new_head = new ListNode(head->val);
+    auto dummy_node = new_head;
+
+    auto current_head = head->next;
+    int counter = 1;
+
+    while (current_head != nullptr) {
+        if (counter == length - n) {
+            current_head = current_head->next;
+            counter++;
+            continue;
+        }
+        dummy_node->next = new ListNode(current_head->val);
+        dummy_node = dummy_node->next;
+        counter++;
+        current_head = current_head->next;
+    }
+
+    return new_head;
+}
---
+++ src/headers/remove_nth_node_from_end_of_list.h
@@ -0,0 +1,14 @@
+﻿#ifndef REMOVE_NTH_NODE_FROM_END_OF_LIST_H
+#define REMOVE_NTH_NODE_FROM_END_OF_LIST_H
+
+#include "../helper/ListNode.h"
+
+
+class remove_nth_node_from_end_of_list {
+public:
+    static ListNode* removeNthFromEnd(ListNode* head, int n);
+};
+
+
+
+#endif //REMOVE_NTH_NODE_FROM_END_OF_LIST_H
