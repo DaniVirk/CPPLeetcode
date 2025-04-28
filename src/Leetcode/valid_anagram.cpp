7e49364d6d0d43561f69d46822e2012d3d73c0d5
Danial <dani2630dk@gmail.com>
2025-04-28 18:21:14 +0200

    isAnagram

---
+++ src/Leetcode/valid_anagram.cpp
@@ -0,0 +1,12 @@
+﻿#include "../headers/valid_anagram.h"
+
+#include <algorithm>
+
+bool valid_anagram::isAnagram(string s, string t) {
+    std::sort(t.begin(), t.end());
+    std::sort(s.begin(), s.end());
+    if (s == t) return true;
+    return false;
+}
+
+
---
+++ src/headers/valid_anagram.h
@@ -0,0 +1,14 @@
+﻿#ifndef VALID_ANAGRAM_H
+#define VALID_ANAGRAM_H
+#include <string>
+
+using std::string;
+
+class valid_anagram {
+public:
+    bool isAnagram(string s, string t);
+};
+
+
+
+#endif //VALID_ANAGRAM_H
